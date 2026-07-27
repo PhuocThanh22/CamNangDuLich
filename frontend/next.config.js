95a03a246d640bd9187575b43a03faddee384b25
Phuoc Thanh <tranvanphuocthanh2106@gmail.com>
2026-07-27 14:00:22 +0700

    fix: add favicon.svg to fix 404 error on favicon.ico

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: '/favicon.ico',
+        destination: '/favicon.svg',
+      },
+      {
         source: '/api/:path*',
         destination: 'https://foodmap-api-osdq.onrender.com/api/:path*',
       },
